e15092de8f3d553be5f3da08ac1c92c0c14ee937
Louis Chen <louischen2021uchi@gmail.com>
2025-04-23 22:02:00 +0800

    update preprocessing

---
+++ src/preprocessing/preprocessing.py
@@ -0,0 +1,72 @@
+import re 
+from dateutil import parser
+def is_valid_date(date_str):
+    try:
+        parser.parse(date_str)
+        return True
+    except:
+        return False
+
+def clean_data(text):
+    text=text.lower()
+    text = ' '.join([w for w in text.split() if not is_valid_date(w)])
+    text = re.sub(r"received from:",' ',text)
+    text = re.sub(r"from:",' ',text)
+    text = re.sub(r"to:",' ',text)
+    text = re.sub(r"subject:",' ',text)
+    text = re.sub(r"sent:",' ',text)
+    text = re.sub(r"ic:",' ',text)
+    text = re.sub(r"cc:",' ',text)
+    text = re.sub(r"bcc:",' ',text)
+    #Remove email 
+    text = re.sub(r'\S*@\S*\s?', '', text)
+    # Remove numbers 
+    text = re.sub(r'\d+','' ,text)
+    # Remove Non Dictionary character-TODO
+    # Remove new line characters 
+    text = re.sub(r'\n',' ',text)
+    # Remove hashtag while keeping hashtag text
+    text = re.sub(r'#','', text)
+    #& 
+    text = re.sub(r'&;?', 'and',text)
+    # Remove HTML special entities (e.g. &amp;)
+    text = re.sub(r'\&\w*;', '', text)
+    # Remove hyperlinks
+    text = re.sub(r'https?:\/\/.*\/\w*', '', text)  
+    # Remove characters beyond Readable formart by Unicode:
+    text= ''.join(c for c in text if c <= '\uFFFF') 
+    text = text.strip()
+    # Remove unreadable characters  (also extra spaces)
+    text = ' '.join(re.sub("[^\u0030-\u0039\u0041-\u005a\u0061-\u007a]", " ", text).split())
+    for name in callers:
+      namelist = [part for part in name.split()]
+      for namepart in namelist: 
+          text = text.replace(namepart,'')
+          
+    text = re.sub(r"\s+[a-zA-Z]\s+", ' ', text)
+    text = re.sub(' +', ' ', text)
+    text = text.strip()
+    return text
+
+import nltk
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer,PorterStemmer
+from nltk.corpus import stopwords
+import re
+lemmatizer = WordNetLemmatizer()
+stemmer = PorterStemmer() 
+
+def preprocess(sentence):
+    sentence=str(sentence)
+    sentence = sentence.lower()
+    sentence=sentence.replace('{html}',"") 
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', sentence)
+    rem_url=re.sub(r'http\S+', '',cleantext)
+    rem_num = re.sub('[0-9]+', '', rem_url)
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(rem_num)  
+    filtered_words = [w for w in tokens if len(w) > 2 if not w in stopwords.words('english')]
+    stem_words=[stemmer.stem(w) for w in filtered_words]
+    lemma_words=[lemmatizer.lemmatize(w) for w in stem_words]
+    return " ".join(filtered_words)
